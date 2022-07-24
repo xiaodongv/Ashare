4d5729e94c988c7ab1639ad6cc59314c9bd531b8
xiaodong <xiaodong@biomap.com>
2022-07-24 10:09:30 +0800

    code optimization

--- Ashare.py
+++ Ashare.py
@@ -25,35 +25,46 @@ def get_price_day_tx(code, end_date='', count=10, frequency='1d'):     # 日线�
 
 # 腾讯分钟线
 def get_price_min_tx(code, end_date=None, count=10, frequency='1d'):    # 分钟线获取 
-    ts=int(frequency[:-1]) if frequency[:-1].isdigit() else 1           # 解析K线周期数
-    if end_date: end_date=end_date.strftime('%Y-%m-%d') if isinstance(end_date,datetime.date) else end_date.split(' ')[0]        
-    URL=f'http://ifzq.gtimg.cn/appstock/app/kline/mkline?param={code},m{ts},,{count}' 
-    st= json.loads(requests.get(URL).content);       buf=st['data'][code]['m'+str(ts)] 
-    df=pd.DataFrame(buf,columns=['time','open','close','high','low','volume','n1','n2'])   
-    df=df[['time','open','close','high','low','volume']]    
-    df[['open','close','high','low','volume']]=df[['open','close','high','low','volume']].astype('float')
-    df.time=pd.to_datetime(df.time);   df.set_index(['time'], inplace=True);   df.index.name=''          #处理索引     
-    df['close'][-1]=float(st['data'][code]['qt'][code][3])                #最新基金数据是3位的
+    ts = int(frequency[:-1]) if frequency[:-1].isdigit() else 1           # 解析K线周期数
+    if end_date:
+        end_date = end_date.strftime('%Y-%m-%d') if isinstance(end_date, datetime.date) else end_date.split(' ')[0]        
+    URL = f'http://ifzq.gtimg.cn/appstock/app/kline/mkline?param={code},m{ts},,{count}' 
+    st = json.loads(requests.get(URL).content)
+    buf = st['data'][code]['m'+str(ts)] 
+    df = pd.DataFrame(buf, columns=['time', 'open', 'close', 'high', 'low', 'volume', 'n1', 'n2'])   
+    df = df[['time', 'open', 'close', 'high', 'low', 'volume']]    
+    df[['open', 'close', 'high', 'low', 'volume']] = df[['open', 'close', 'high', 'low', 'volume']].astype('float')
+    df.time = pd.to_datetime(df.time) 
+    df.set_index(['time'], inplace=True)
+    df.index.name = ''          # 处理索引     
+    df['close'][-1] = float(st['data'][code]['qt'][code][3])                # 最新基金数据是3位的
     return df
 
 
 # sina新浪全周期获取函数，分钟线 5m,15m,30m,60m  日线1d=240m   周线1w=1200m  1月=7200m
-def get_price_sina(code, end_date='', count=10, frequency='60m'):    #新浪全周期获取函数    
-    frequency=frequency.replace('1d','240m').replace('1w','1200m').replace('1M','7200m');   mcount=count
-    ts=int(frequency[:-1]) if frequency[:-1].isdigit() else 1       #解析K线周期数
-    if (end_date!='') & (frequency in ['240m','1200m','7200m']): 
-        end_date=pd.to_datetime(end_date) if not isinstance(end_date,datetime.date) else end_date    #转换成datetime
-        unit=4 if frequency=='1200m' else 29 if frequency=='7200m' else 1    #4,29多几个数据不影响速度
-        count=count+(datetime.datetime.now()-end_date).days//unit            #结束时间到今天有多少天自然日(肯定 >交易日)        
-        #print(code,end_date,count)    
+def get_price_sina(code, end_date='', count=10, frequency='60m'):    # 新浪全周期获取函数    
+    frequency = frequency.replace('1d', '240m').replace('1w', '1200m').replace('1M', '7200m')
+    mcount = count
+    ts = int(frequency[:-1]) if frequency[:-1].isdigit() else 1       # 解析K线周期数
+    if (end_date != '') & (frequency in ['240m', '1200m', '7200m']): 
+        end_date = pd.to_datetime(end_date) if not isinstance(end_date, datetime.date) else end_date    # 转换成datetime
+        unit = 4 if frequency == '1200m' else 29 if frequency == ' 7200m' else 1    # 4,29多几个数据不影响速度
+        count = count+(datetime.datetime.now()-end_date).days//unit            # 结束时间到今天有多少天自然日(肯定 >交易日)        
+        # print(code,end_date,count)    
     URL = f'http://money.finance.sina.com.cn/quotes_service/api/json_v2.php/CN_MarketData.getKLineData?symbol={code}&scale={ts}&ma=5&datalen={count}' 
-    dstr = json.loads(requests.get(URL).content);       
+    dstr = json.loads(requests.get(URL).content)      
     # df=pd.DataFrame(dstr,columns=['day','open','high','low','close','volume'],dtype='float') 
-    df = pd.DataFrame(dstr,columns=['day','open','high','low','close','volume'])
-    df['open'] = df['open'].astype(float); df['high'] = df['high'].astype(float);                          #转换数据类型
-    df['low'] = df['low'].astype(float);   df['close'] = df['close'].astype(float);  df['volume'] = df['volume'].astype(float)    
-    df.day = pd.to_datetime(df.day);    df.set_index(['day'], inplace=True);     df.index.name=''            #处理索引                 
-    if (end_date !='') & (frequency in ['240m', '1200m', '7200m']): return df[df.index<=end_date][-mcount:]   #日线带结束时间先返回              
+    df = pd.DataFrame(dstr, columns=['day', 'open', 'high', 'low', 'close', 'volume'])
+    df['open'] = df['open'].astype(float)
+    df['high'] = df['high'].astype(float)                        # 转换数据类型
+    df['low'] = df['low'].astype(float)
+    df['close'] = df['close'].astype(float)
+    df['volume'] = df['volume'].astype(float)    
+    df.day = pd.to_datetime(df.day)
+    df.set_index(['day'], inplace=True)
+    df.index.name = ''            # 处理索引                 
+    if (end_date != '') & (frequency in ['240m', '1200m', '7200m']): 
+        return df[df.index <= end_date][-mcount:]   # 日线带结束时间先返回              
     return df
 
 
